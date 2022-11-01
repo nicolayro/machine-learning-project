efb0fd5901c3d83526e1a81ab85243d72b139316
markuSolli <markusped@hotmail.com>
2022-11-01 14:12:35 +0100

    fixed iteration issue in step method

--- environment.py
+++ environment.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.freetype
+import copy
 import numpy as np
 from time import sleep
 from food import Food
@@ -29,10 +30,12 @@ class Environment:
         self.init_variables()
 
     def step(self):
+        original_grid = copy.deepcopy(self.grid)
+
         for x in range(self.grid_size):
             for y in range(self.grid_size):
-                if self.grid[x][y] != 0:
-                    self.grid[x][y].step((x, y), self)
+                if original_grid[x][y] != 0:
+                    original_grid[x][y].step((x, y), self)
 
     def render(self):
         pygame.init()
@@ -65,18 +68,17 @@ def drawGrid(screen, env):
 
     for x in range(env.grid_size):
         for y in range(env.grid_size):
-            # Draw background
             rect = pygame.Rect(x * block_size, y * block_size, block_size + 1, block_size + 1)
-            pygame.draw.rect(screen, (0, env.background[x][y], 0), rect)
 
+            # Draw food or background
+            if isinstance(env.grid[x][y], Food):
+                pygame.draw.rect(screen, (227, 206, 18), rect)
+            else:
+                pygame.draw.rect(screen, (0, env.background[x][y], 0), rect)
+            
             # Draw individuals
             if isinstance(env.grid[x][y], Individual):
-                pygame.draw.circle(screen, (220, 0, 0), ((x + 0.5) * block_size, ((y + 0.5) * block_size)), block_size / 4)
-            
-            # Draw food
-            if isinstance(env.grid[x][y], Food):
-                rect = pygame.Rect((x + 0.25) * block_size, (y + 0.25) * block_size, block_size / 2, block_size / 2)
-                pygame.draw.rect(screen, (227, 206, 18), rect)
+                pygame.draw.circle(screen, (220, 0, 0), ((x + 0.5) * block_size, ((y + 0.5) * block_size)), block_size / 2)
             
             # Render text
             env.font.render_to(screen, (10, env.screen_size - 24), str(env.tick), (0, 0, 0))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from environment import Environment
 
 if __name__ == '__main__':
-    env = Environment(grid_size=10, fps=5)
+    env = Environment()
     env.render()
